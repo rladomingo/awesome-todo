701fe762353c7a42771024b63eb6e7cddbabaf06
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2022-05-04 02:15:57 +0800

    feat: add CORS

--- server/index.py
+++ server/index.py
@@ -1,13 +1,15 @@
 from flask import Flask, redirect, request, jsonify
 from dotenv import dotenv_values
 from flask_bcrypt import Bcrypt
-from routes import Url 
+from routes import Url
 from user import UserService
 from db import Database
 from util import get_bearer_token
+from flask_cors import CORS
 
-env = dotenv_values(".env") 
+env = dotenv_values(".env")
 app = Flask(__name__)
+cors = CORS(app)
 db = Database(
     env.get("USERNAME"),
     env.get("PASSWORD"),
@@ -17,11 +19,13 @@ bcrypt = Bcrypt(app)
 
 """ START OF USERS API ENDPOINT  """
 
+
 @app.get(Url.USERS_PING)
 def users_ping():
     """ Handle users endpoint health check """
 
     return UserService.ping()
+
 
 @app.post(Url.USERS_REGISTER)
 def users_register():
@@ -34,9 +38,10 @@ def users_register():
         bcrypt.generate_password_hash(request.json.get('password'))
     )
 
+
 @app.post(Url.USERS_LOGIN)
 def users_login():
-    """ Handle user login """ 
+    """ Handle user login """
 
     return UserService.login(
         db,
@@ -46,12 +51,14 @@ def users_login():
         bcrypt.check_password_hash,
     )
 
+
 @app.get(Url.USERS_GET_MYSELF)
 def users_get_myself():
     """ Handle user get info """
 
     token = get_bearer_token(request.headers)
-    return UserService.get_myself(db,token)
+    return UserService.get_myself(db, token)
+
 
 @app.post(Url.USERS_REF_TOKEN)
 def users_ref_token():
@@ -60,12 +67,11 @@ def users_ref_token():
     token = get_bearer_token(request.headers)
     return UserService.refresh_token(token)
 
+
 """ END OF USERS API ENDPOINT """
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
-        app.run(port=env.get("SERVER_PORT"),debug=True)
+        app.run(port=env.get("SERVER_PORT"), debug=True)
     except KeyboardInterrupt:
         db.close_connection()
-    
-

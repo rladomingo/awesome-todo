8b75c79a6233b7547bab3daf6902e7d56220fa89
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2022-05-04 20:29:25 +0800

    fix: seed sql

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,5 +1,5 @@
 -- Begin seed
-DROP DATABASE awesome_todo;
+DROP DATABASE IF EXISTS awesome_todo;
 
 -- Create application database
 CREATE DATABASE awesome_todo;
@@ -9,31 +9,36 @@ USE awesome_todo;
 
 -- Create user table
 CREATE TABLE user (
-    user_id INT PRIMARY KEY AUTO_INCREMENT, 
-    username VARCHAR(64) UNIQUE NOT NULL,
-    email  VARCHAR(256) UNIQUE NOT NULL,
+    user_id INT AUTO_INCREMENT, 
+    username VARCHAR(64) NOT NULL,
+    email  VARCHAR(256) NOT NULL,
     password VARCHAR(256) NOT NULL
+    CONSTRAINT user_userid_pk PRIMARY KEY(user_id) 
+    CONSTRAINT user_username_uk UNIQUE(username)
+    CONSTRAINT user_email_uk UNIQUE(email)
 );
 
 -- Create category table
 CREATE TABLE category (
-    cat_id INT PRIMARY KEY AUTO_INCREMENT,
+    cat_id INT AUTO_INCREMENT,
     name VARCHAR(32) NOT NULL,
     num_of_task INT NOT NULL DEFAULT 0,
     user_id INT NOT NULL,
-    CONSTRAINT cat_user_fk FOREIGN KEY(user_id) REFERENCES user(user_id)
+    CONSTRAINT category_catid_pk PRIMARY KEY(cat_id) 
+    CONSTRAINT cat_userd_fk FOREIGN KEY(user_id) REFERENCES user(user_id)
 );
 
 -- Create task table
 CREATE TABLE task (
-    task_id INT PRIMARY KEY AUTO_INCREMENT,
+    task_id INT AUTO_INCREMENT,
     title TEXT NOT NULL,
     description TEXT,
     completed BOOLEAN NOT NULL DEFAULT 0,
     due_date DATE,
     user_id INT NOT NULL,
     cat_id INT,
-    CONSTRAINT task_user_fk FOREIGN KEY(user_id) REFERENCES user(user_id),
-    CONSTRAINT task_cat_fk FOREIGN KEY(cat_id) REFERENCES category(cat_id)
+    CONSTRAINT task_taskid_pk PRIMARY KEY(task_id) 
+    CONSTRAINT task_userid_fk FOREIGN KEY(user_id) REFERENCES user(user_id),
+    CONSTRAINT task_catid_fk FOREIGN KEY(cat_id) REFERENCES category(cat_id)
 );
 

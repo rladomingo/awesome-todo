0e9bc13bf12a8bea6d3349622faa372a9c6cc318
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2022-05-04 21:04:54 +0800

    feat: added build files for sql

--- sql/build.sh
+++ sql/build.sh
@@ -1,6 +1,10 @@
-#! /usr/bin/bash
+#! /bin/bash
 
+rm calma_divino_domingo_s4l_milestone3.sql
 cat seed.sql >> calma_divino_domingo_s4l_milestone3.sql
+echo "" >> calma_divino_domingo_s4l_milestone3.sql
 cat user.sql >> calma_divino_domingo_s4l_milestone3.sql
+echo "" >> calma_divino_domingo_s4l_milestone3.sql
 cat category.sql >> calma_divino_domingo_s4l_milestone3.sql
+echo "" >> calma_divino_domingo_s4l_milestone3.sql
 cat task.sql >> calma_divino_domingo_s4l_milestone3.sql
---
+++ sql/clean.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+ 
+ rm calma_divino_domingo_s4l_milestone3.sql

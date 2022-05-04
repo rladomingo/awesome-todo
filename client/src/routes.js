5dce5d1b2b682f3d7e7bdade08bf805308361b88
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2022-05-05 00:11:42 +0800

    feat: add auth routes

---
+++ client/src/routes.js
@@ -0,0 +1,18 @@
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+export const routes = [
+  {
+    path: '/register',
+    element: Register,
+  },
+  {
+    path: '/login',
+    element: Login,
+  },
+  {
+    path: '/',
+    element: Home,
+  },
+]

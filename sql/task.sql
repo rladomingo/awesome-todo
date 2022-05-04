b5a9e8781a0cc749cdde0a7ef375baf9318fce67
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2022-05-04 21:05:01 +0800

    feat: added task queries

--- sql/task.sql
+++ sql/task.sql
@@ -1,13 +1,72 @@
--- Create task
+-- Insert tasks for Juan Dela Cruz
+INSERT INTO task (
+    title,
+    due_date,
+    user_id
+) VALUES (
+    'This task should be done by June 1, 2022',
+    '2022-06-01',
+    1
+);
+INSERT INTO task (
+    title,
+    due_date,
+    user_id
+) VALUES (
+    'This task should be done by June 4, 2022',
+    '2022-06-04',
+    1
+);
+INSERT INTO task (
+    title,
+    user_id
+) VALUES (
+    'This task has no due date',
+    1
+);
+INSERT INTO task (
+    title,
+    description,
+    user_id
+) VALUES (
+    'This task has a title',
+    'This task has a description',
+    1
+);
 
--- Edit task
 
--- Delete task
+-- Update task to have a due date owned by Juan Dela Cruz
+UPDATE task 
+SET due_date = '2022-06-07'
+WHERE user_id = 1 AND task_id = 4;
 
--- View task (all)
+-- Update task to have a description owned by Juan Dela Cruz
+UPDATE task 
+SET description = 'Blah blah'
+WHERE user_id = 1 AND task_id = 1;
 
--- Mark task as done
+-- Delete a task owned by Juan Dela Cruz
+DELETE FROM task 
+WHERE user_id = 1 AND task_id = 2;
+
+-- View all Juan Dela Cruz's tasks
+SELECT * FROM task WHERE user_id = 1;
+
+-- Mark task as done owned by Juan Dela Cruz
+UPDATE task 
+SET completed = 1
+WHERE user_id = 1 AND task_id = 3;
 
 -- Add task to a category
+-- TODO: if category is available
 
--- View task with filter (per day, month)
+-- View task all tasks owned by Juan Dela Cruz due for June
+SELECT * FROM task 
+WHERE user_id = 1 
+HAVING MONTH(due_date) = 6;
+
+
+-- View task all tasks owned by Juan Dela Cruz due for Monday
+SELECT * FROM task 
+WHERE user_id = 1 
+HAVING WEEKDAY(due_date) = 1;

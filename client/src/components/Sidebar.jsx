5b7ca62f6b55776842f9a5d0d2f7f496034181e1
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2022-05-31 19:34:50 +0800

    feat: implement display all my categories

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,36 @@
 import { Sidebar as GrommetSidebar, Button, Nav } from 'grommet'
+import { useEffect, useState } from 'react'
+import { retrieveMyCategories } from '../apis/category'
 import CreateCategory from './CreateCategory'
 import Profile from './Profile'
 
 export default function Sidebar(props) {
+  const [categories, setCategories] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        setCategories(await retrieveMyCategories())
+        setError(null)
+      } catch (err) {
+        setCategories(null)
+        setError(String(err))
+      } finally {
+        setLoading(false)
+      }
+    })()
+  }, [])
+
+  if (error) {
+    return <div>{error}</div>
+  }
+
+  if (loading) {
+    return <div>loading...</div>
+  }
+
   return (
     <GrommetSidebar
       background="brand"
@@ -11,8 +39,15 @@ export default function Sidebar(props) {
       footer={<CreateCategory />}
     >
       <Nav gap="small">
-        <Button primary hoverIndicator label="Category 1" />
-        <Button primary hoverIndicator label="Category 1" />
+        {categories &&
+          categories.map(category => (
+            <Button
+              key={category.cat_id}
+              primary
+              hoverIndicator
+              label={category.name}
+            />
+          ))}
       </Nav>
     </GrommetSidebar>
   )
